9929e31e6c732387879d18c43835f84ce86a037e
Dave Carlson <dcarlson@ClinicalCloud.solutions>
2021-05-27 09:50:08 -0600

    Updates

---
+++ precache-manifest.5118d21e253ac8146d0d37e455765fb7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd086c552bc0f4a0b645b7acb059d14",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/index.html"
+  },
+  {
+    "revision": "93fa3502f74aadaeeb08",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/css/main.ba6a6dc6.chunk.css"
+  },
+  {
+    "revision": "e3dd5f21378b47c94875",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/0.9dfa8d14.chunk.js"
+  },
+  {
+    "revision": "d8436c03c4cdfe51e334c2bdf96bd0b8",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/0.9dfa8d14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dedbd15c7be4b5cfd8a",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/5.22373a93.chunk.js"
+  },
+  {
+    "revision": "9df7e02516226031a3d6727c22ea30f5",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/5.22373a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591a9929a3639a78463b",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/launch.c2fa3910.chunk.js"
+  },
+  {
+    "revision": "93fa3502f74aadaeeb08",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/main.84be5d9b.chunk.js"
+  },
+  {
+    "revision": "f229efe4673b0a85e72a",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/runtime-launch.0fec6c9c.js"
+  },
+  {
+    "revision": "57bf10b2bb95cf8d3f1e",
+    "url": "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/static/js/runtime-main.f4134595.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/precache-manifest.ff8ce68a45401445935796adffad8f13.js"
+  "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/precache-manifest.5118d21e253ac8146d0d37e455765fb7.js"
 );
 
 self.addEventListener('message', (event) => {
