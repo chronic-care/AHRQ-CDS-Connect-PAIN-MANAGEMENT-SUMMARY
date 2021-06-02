ed70d3011995ec131482ecddd26cc286faad54df
Dave Carlson <dcarlson@ClinicalCloud.solutions>
2021-06-02 14:40:03 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/precache-manifest.63132d549e06c00b6cbdb0fde31c5dfb.js"
+  "/AHRQ-CDS-Connect-PAIN-MANAGEMENT-SUMMARY/precache-manifest.94fd2d9e2c190119dd1cd7f78fabe29d.js"
 );
 
 self.addEventListener('message', (event) => {
